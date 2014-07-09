15c4688e6146ea355a67c306b9a7d6c72a615cc0
Kevin Lin <kevin.lin.p@gmail.com>
2014-07-08 22:06:18 -0700

    Concentric circles, now skewing.

--- squares.rb
+++ squares.rb
@@ -18,19 +18,21 @@ rvg = RVG.new(10.in, 16.in).viewbox(-50, -80, 100, 160) do |c|
   c.background_fill = 'white'
 
   num_circles = []
-  20.times do |n|
+  16.times do |n|
     num_circles << 20 * n
   end
 
   num_circles.each_with_index do |num_circle, series|
-    last_circle_index = num_circle - 1
-    a = num_circle - 1
-    diameter = series * 1
+    radius = series * 1
 
-    (0..last_circle_index).each do |a|
-      change = Math.sin((a + (series * 100))/num_circle.to_f * 2*Math::PI) * 0.2 + 0.8
-      x = Math.sin(a/num_circle.to_f * 2*Math::PI) * diameter * change
-      y = Math.cos(a/num_circle.to_f * 2*Math::PI) * diameter * change
+    (0...num_circle).each do |a|
+      #change = Math.sin((a + (series * 100))/num_circle.to_f * 2*Math::PI) * 0.2 + 0.8
+      x = Math.sin(a/num_circle.to_f * 2*Math::PI) * radius
+      y = Math.cos(a/num_circle.to_f * 2*Math::PI) * radius
+
+      skew_factor = 1.0 * 2
+      x += Math.sin(a/num_circle.to_f * 2*Math::PI) * ((series % 2)/skew_factor)
+      y += Math.cos(a/num_circle.to_f * 2*Math::PI) * (((series + 1) % 2)/skew_factor)
 
       c.g.translate(x, y) do |g|
         g.styles stroke: 'black', stroke_width: 0.1, fill: 'white'
