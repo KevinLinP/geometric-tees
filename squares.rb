62a75168f5b685d6617743126c9eabd00a144e47
Kevin Lin <kevin.lin.p@gmail.com>
2014-07-13 22:23:05 -0700

    Concentric circles: added rudimentary color and rotation.

--- squares.rb
+++ squares.rb
@@ -1,4 +1,5 @@
 require 'rvg/rvg'
+require 'color'
 include Magick
 include Math
 
@@ -18,24 +19,46 @@ rvg = RVG.new(10.in, 16.in).viewbox(-50, -80, 100, 160) do |c|
   c.background_fill = 'white'
 
   num_circles = []
-  16.times do |n|
-    num_circles << 20 * n
+  iterations = 50
+  iterations.times do |n|
+    num_circles << 5 * n
   end
 
   num_circles.each_with_index do |num_circle, series|
-    radius = series * 1
+    radius = series * 0.9
 
     (0...num_circle).each do |a|
       #change = Math.sin((a + (series * 100))/num_circle.to_f * 2*Math::PI) * 0.2 + 0.8
       x = Math.sin(a/num_circle.to_f * 2*Math::PI) * radius
       y = Math.cos(a/num_circle.to_f * 2*Math::PI) * radius
 
-      skew_factor = 1.0 * 2
-      x += Math.sin(a/num_circle.to_f * 2*Math::PI) * ((series % 2)/skew_factor)
-      y += Math.cos(a/num_circle.to_f * 2*Math::PI) * (((series + 1) % 2)/skew_factor)
+      root = 4.0
 
-      c.g.translate(x, y) do |g|
-        g.styles stroke: 'black', stroke_width: 0.1, fill: 'white'
+      hue = (a / (1.0 * num_circle)) * (360 * 1)
+      #hue = hue % 360
+
+      if (series % 2) == 0
+        sin = Math.sin(a/num_circle.to_f * 2*Math::PI)
+        
+        root_sin = sin.abs ** (1/root)
+        root_sin *= -1.0 if sin < 0
+        x += root_sin
+
+        hue = (hue + 180) % 360
+      else
+        cos = Math.cos(a/num_circle.to_f * 2*Math::PI)
+
+        root_cos = cos.abs ** (1/root)
+        root_cos *= -1.0 if cos < 0
+        y += root_cos
+      end
+
+      color = "hsl(#{hue}, 100, 100)"
+
+      rotation = (a / (-1.0 * num_circle)) * 360
+
+      c.g.translate(x, y).rotate(rotation) do |g|
+        g.styles stroke: 'white', stroke_width: 0.05, fill: color
         g.polygon [0,0, 1,0, 1,1, 0,1]
       end
     end
