49e7c1af0c3a34987701ae1dfa695581cb5b2b90
Kevin Lin <kevin.lin.p@gmail.com>
2014-07-28 00:16:39 -0700

    Concentric circles: Brightened color. Changed outline to white per Alex's suggestion. Color rotations changed.

--- squares.rb
+++ squares.rb
@@ -15,7 +15,7 @@ def periodic_variant(coordinate, period, multipliers)
 end
 
 rvg = RVG.new(10.in, 16.in).viewbox(-50, -80, 100, 160) do |c|
-  c.background_fill = 'black'
+  c.background_fill = 'white'
 
   num_circles = []
   iterations = 50
@@ -25,7 +25,8 @@ rvg = RVG.new(10.in, 16.in).viewbox(-50, -80, 100, 160) do |c|
 
   num_circles.each_with_index do |num_circle, series|
     next if series <= 1
-    radius = series * 0.75
+    radius_increment = 0.65
+    radius = series * radius_increment
 
     (0...num_circle).each do |a|
       #change = Math.sin((a + (series * 100))/num_circle.to_f * 2*Math::PI) * 0.2 + 0.8
@@ -34,14 +35,17 @@ rvg = RVG.new(10.in, 16.in).viewbox(-50, -80, 100, 160) do |c|
 
       root = 2.0
 
-      hue = (a / (1.0 * num_circle)) * (360 * 1)
+      #color_rotations_array = [2, 2, 3, 3]
+      #color_rotations = color_rotations_array[3]
+      #color_rotations = (series % 2) + 2 # 2-3 rotations
+      hue = (a / (1.0 * num_circle)) * (360 * 3) 
 
       if (series % 2) == 0
         sin = Math.sin(a/num_circle.to_f * 2*Math::PI)
         
         root_sin = sin.abs ** (1/root)
         root_sin *= -1.0 if sin < 0
-        x += root_sin
+        x += (root_sin * radius_increment)
 
         hue = (hue + 180) % 360
       else
@@ -49,18 +53,21 @@ rvg = RVG.new(10.in, 16.in).viewbox(-50, -80, 100, 160) do |c|
 
         root_cos = cos.abs ** (1/root)
         root_cos *= -1.0 if cos < 0
-        y += root_cos
+        y += (root_cos * radius_increment)
       end
 
       color_rotation = series % 6
-      hue = hue + (30 * (series % 12))
+      offset = 16
+      offset_length = 360 / offset
+      hue = hue + (offset * (series % offset_length))
 
-      color = "hsl(#{hue}, 100, 100)"
+      color = "hsl(#{hue}, 100, 125)"
 
       rotation = (a / (-1.0 * num_circle)) * 360
 
       c.g.translate(x, y).rotate(rotation) do |g|
-        g.styles stroke: 'white', stroke_width: 0.05, fill: color
+        g.styles fill: color, stroke: 'white', stroke_width: 0.10 # thinner white borders on the outside
+        #g.styles fill: color # thinner white borders on the outside
         g.polygon [0.5,0.5, 0.5,-0.5, -0.5,-0.5, -0.5,0.5]
       end
     end
